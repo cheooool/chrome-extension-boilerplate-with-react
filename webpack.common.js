51530e6071882b57cd13bde364dfd2ade1cd6bd8
Hoecheol <g.cheooool@gmail.com>
2022-02-17 18:58:30 +0900

    Update webpack.common.js

--- webpack.common.js
+++ webpack.common.js
@@ -23,7 +23,7 @@ module.exports = {
         exclude: /node_modules/,
       },
       {
-        test: /\.s[ac]ss$/i,
+        test: /\.css|\.s[ac]ss$/i,
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
     ],
